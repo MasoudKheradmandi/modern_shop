efd30f70a62285a443b2513fd74a82db1eca659e
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2024-01-30 14:40:43 +0330

    create Product models and depend

--- core/product/models.py
+++ core/product/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100,verbose_name='نام دسته بندی')
+    image = models.ImageField(verbose_name='عکس دسته بندی')
+    
+    def __str__(self):
+        return self.name
+
+
+class DiscountCode(models.Model):
+    count = models.PositiveIntegerField() # تعداد کد تخفیف برای محصول ->۵ نفر میتوانند استفاده کنند
+    code_name = models.CharField(max_length=30)
+    discountـpercent= models.PositiveIntegerField(validators=[MaxValueValidator(100),MinValueValidator(0)]) # درصد تخفیف
+    
+    def __str__(self):
+        return self.name + " " + self.count
+
+
+class TvSize(models.Model):
+    product = models.ForeignKey("Product",on_delete=models.PROTECT)
+    size = models.CharField(max_length=4,verbose_name='سایز')
+    price_difference = models.IntegerField(verbose_name='اختلاف قیمت',default=0)
+    count = models.PositiveIntegerField() # تعداد تلویزیون با سایز مشخص یا همون موجودی انبار
+    discount = models.OneToOneField(DiscountCode,on_delete=models.SET_NULL,null=True)
+    
+    def __str__(self):
+        return self.product.name + " " + self.size
+    
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True, default=1000)
+    name = models.CharField(max_length=450,verbose_name='نام محصول')
+    image = models.ImageField(verbose_name="عکس محصول")
+    image_1 = models.ImageField(verbose_name="عکس محصول",null=True)
+    image_2 = models.ImageField(verbose_name="عکس محصول",null=True)
+    image_3 = models.ImageField(verbose_name="عکس محصول",null=True)
+
+
+    category = models.ForeignKey(Category,on_delete=models.PROTECT)
+    sales_number = models.IntegerField(default=0) #تعداد فروش 
+    price = models.PositiveIntegerField()
+
+    specification = models.TextField()
+    description = models.TextField()
+
+    quality = models.IntegerField(null=True)
+    pannel_type = models.CharField(max_length=250,null=True)
+    port = models.CharField(max_length=450,null=True)
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True,null=True)
+    is_special = models.BooleanField(default=False)
+    is_show = models.BooleanField(default=True)
+
+    
+    
+    def __str__(self):
+        return self.name
