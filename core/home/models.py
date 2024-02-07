de099c592ff3fd7690ff2fd0a01ae356322f82db
erfan-karimi <h410mi3@gmail.com>
2024-02-07 10:41:41 +0330

    render partial footer and header

--- core/home/models.py
+++ core/home/models.py
@@ -1,9 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+
+class SiteSettings(models.Model):
+    top_banner = models.ImageField()
+    site_logo = models.ImageField()
+    left_sidebar_banner_1 = models.ImageField()
+    left_sidebar_banner_2 = models.ImageField()
+    footer_seo_text = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+
+class Slider(models.Model):
+    image = models.ImageField()
+
+
 class NewsLatter(models.Model):
     email = models.EmailField(unique=True)
 
 
     def __str__(self):
         return self.email
+
+
+class Navbar(models.Model):
+    name = models.CharField(max_length=250)
+    link = models.CharField(max_length=250, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Footer(models.Model):
+    name = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.name
+
+
+class SubFooter(models.Model):
+    parent = models.ForeignKey(Footer,on_delete=models.CASCADE,null=True)
+    name = models.CharField(max_length=250)
+    link = models.CharField(max_length=250,default="#")
+
+
+    def __str__(self):
+        return self.name
--- core/home/urls.py
+++ core/home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from home.views import HomeView
+from home.views import HomeView , HeaderView , FooterView
 
 urlpatterns = [
     path('', HomeView.as_view(),name='index-page'),
+    path('header/', HeaderView.as_view(),name='header-layout'),
+    path('footer/', FooterView.as_view(),name='footer-layout'),
+
 ]
