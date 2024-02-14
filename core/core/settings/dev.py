e867b68d3306069f9a46e5d1ece34871eb5e7442
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2024-02-15 00:06:54 +0330

    fix settings

---
+++ core/core/settings/dev.py
@@ -0,0 +1,5 @@
+from .base import *
+
+
+SECRET_KEY = 'django-insecure-ljt_%&%yy93!^b27+ev+nx*c_pboq%)lx)&xhml60)3ga!46+s'
+DEBUG = True
--- core/manage.py
+++ core/manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    if os.environ.get('is_production') == 'true':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.prod')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.dev')
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
