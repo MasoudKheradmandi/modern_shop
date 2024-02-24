8f3465c8041677f8e5e9c0b4244c8bd831c8ae33
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2024-02-24 04:12:44 +0330

    Update prod.py

--- core/core/settings/prod.py
+++ core/core/settings/prod.py
@@ -2,6 +2,6 @@ from .base import *
 from dotenv import load_dotenv
 
 load_dotenv()
-ALLOWED_HOSTS =  os.environ.get('ALLOWED_HOSTS').split(' ')
-SECRET_KEY= os.environ.get('SECRET_KEY')
-DEBUG =  os.environ.get('DEBUG')
+ALLOWED_HOSTS =  os.environ.get('ALLOWED_HOSTS','* ').split(' ')
+SECRET_KEY= os.environ.get('SECRET_KEY','test') 
+DEBUG =  os.environ.get('DEBUG',False)
