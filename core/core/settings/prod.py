4ed589c5b7b9e2da5e3bfaab25da1f07978f79da
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2024-03-05 13:08:54 +0330

    add redis Configure to Production setting

--- core/core/settings/prod.py
+++ core/core/settings/prod.py
@@ -13,3 +13,12 @@ SECRET_KEY = config("SECRET_KEY", default="test")
 DEBUG = config("DEBUG", cast=bool, default=True)
 
 MERCHANTID = config("MerchantID",default="test")
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://redis:6379/",
+        "KEY_PREFIX": "html_",
+        "TIMEOUT": 60 * 15,  # in seconds: 60 * 15 (15 minutes)
+    }
+}
