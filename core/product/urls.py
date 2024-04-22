234e28b1bec420b26ede5751e979a4a465f5832d
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2024-04-22 21:02:18 +0330

    add search

--- core/product/urls.py
+++ core/product/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('listview/',views.ProductListView.as_view(),name='listview'),
     path('<str:category>/<int:id>/',views.ProductDetailView.as_view(),name='detail'),
     path('wishlist/',views.WishListView.as_view(),name='wish_list'),
-
+    path('search/',views.Search.as_view(),name='search'),
     path('log/',views.login_view,),
 ]
