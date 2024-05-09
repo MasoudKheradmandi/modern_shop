d0b2e96dfb77d3dd3d085192b8a9188fbaa4ea9d
erfan-karimi <100835460+erfan-karimii@users.noreply.github.com>
2024-05-09 12:20:27 +0330

    add requied variable for ssl in prod.py

--- core/core/settings/prod.py
+++ core/core/settings/prod.py
@@ -12,4 +12,8 @@ SECRET_KEY = config("SECRET_KEY", default="test")
 
 DEBUG = config("DEBUG", cast=bool, default=False)
 
+CSRF_COOKIE_SECURE = config("CSRF_COOKIE_SECURE", cast=bool, default=False)
+CSRF_USE_SESSIONS = config("CSRF_USE_SESSIONS", cast=bool, default=False)
+CSRF_COOKIE_DOMAIN = config("CSRF_USE_SESSIONS",default='*')
+
 MERCHANTID = config("MerchantID",default="test")
